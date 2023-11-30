34a04625d89fad12a86e6e4059e8a66ae953fc6c
mayvenmegan <megan@shopmayven.com>
2023-11-30 22:26:10 +0530

    fixing some styles

--- app/page.js
+++ app/page.js
@@ -1,56 +1,74 @@
-'use client'
+'use client';
 import { useRouter } from 'next/navigation';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import Header from '../components/header';
 import Footer from '../components/footer';
 import { InstantSearch, SearchBox } from 'react-instantsearch';
-import searchClient from '../components/algolia'
+import searchClient from '../components/algolia';
 
-import replaceAndRemoveChar from "../utils/replaceAndRemoveChar"
+import replaceAndRemoveChar from '../utils/replaceAndRemoveChar';
 import Link from 'next/link';
 
 export default function Home() {
-  const router = useRouter()
-  const [searchQuery, setSearchQuery] = useState('')
+  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState('');
   const handleChange = (e) => {
     // console.log(e.uiState.dev_MAYVEN.query);
-    const query=e.uiState.dev_MAYVEN.query;
+    const query = e.uiState.dev_MAYVEN.query;
 
     setSearchQuery(replaceAndRemoveChar(query));
-  }
+  };
   return (
-
-    <InstantSearch searchClient={searchClient} indexName='dev_MAYVEN' onStateChange={handleChange}>
+    <InstantSearch
+      searchClient={searchClient}
+      indexName='dev_MAYVEN'
+      onStateChange={handleChange}
+    >
       <Header />
       <div className='search-section'>
-        <SearchBox placeholder='What are you looking for?' onSubmit={() => router.push(`search/${searchQuery}`)}/>
+        <SearchBox
+          placeholder='What are you looking for?'
+          onSubmit={() => router.push(`search/${searchQuery}`)}
+        />
 
-        <button className='search-btn' onClick={() => router.push(`search/${searchQuery}`)}> 
-        Find it!
+        <button
+          className='search-btn'
+          onClick={() => router.push(`search/${searchQuery}`)}
+        >
+          Find it!
         </button>
       </div>
       <div className='search-sugges-box'>
-      <Link
-      href={`/search/female-founder-suncreen-spf-50`}
-      className='homepage-search-suggestion'
-      >
-        <button >Female Founder <div>Sunscreen SPF 50</div> </button>
-        </Link>
+        <div>
+          <Link
+            href={`/search/female-founder-suncreen-spf-50`}
+            className='homepage-search-suggestion'
+          >
+            <button>
+              Female Founder <div>Sunscreen SPF 50</div>{' '}
+            </button>
+          </Link>
+          <Link
+            href={`/search/vegan-shampoo-made-in-usa`}
+            className='homepage-search-suggestion'
+          >
+            <button>
+              Vegan Shampoo <div>Made in the USA</div>{' '}
+            </button>
+          </Link>
+        </div>
         <Link
-         href={`/search/vegan-shampoo-made-in-usa`}
-         className='homepage-search-suggestion'
+          href={`/search/anti-cruelty-baby-lotion`}
+          className='homepage-search-suggestion'
         >
-        <button >Vegan Shampoo <div>Made in the USA</div> </button>
-        </Link>
-        <Link
-         href={`/search/anti-cruelty-baby-lotion`}
-         className='homepage-search-suggestion'>
-        <button >Anti Cruelty <div>Baby Lotion</div> </button>
+          <button>
+            Anti Cruelty <div>Baby Lotion</div>{' '}
+          </button>
         </Link>
       </div>
       <div className='footer'>
-      <Footer />
+        <Footer />
       </div>
     </InstantSearch>
   );
