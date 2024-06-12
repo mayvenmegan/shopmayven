de79277287635ad2be9e33227c360dfdd3110cc3
Somveer Kumar <main.somveerkumar@gmail.com>
2024-06-12 21:43:41 +0530

    adding dynamic metadata

--- app/product/[productId]/page.js
+++ app/product/[productId]/page.js
@@ -1,64 +1,18 @@
-'use client';
-import { useRouter } from "next/navigation";
-import React, {useState, useEffect} from 'react';
-import { FaLessThan } from "react-icons/fa6";
-import { useSearchParams } from 'next/navigation'
+import ProductPage from "@/components/ProductPage/ProductPage";
+import replaceAndRemoveDash from "@/utils/replaceAndRemoveDash";
 
-const algoliasearch = require("algoliasearch");
-const searchClient = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY
-  );
-const index = searchClient.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME);
+export async function generateMetadata ({params}) {
+  return {
+    title: replaceAndRemoveDash(params.productId) + " ~ Product",
+    description: `It is the product page of ${params.productId} product on shopmayven`,
+  }
+}
 
-import ProductPageLeft from "@/components/ProductPage/ProductPageLeft";
-import ProductPageRight from "@/components/ProductPage/ProductPageRight";
-
-
-export default function ProductPage () {
-  const [product, setProduct] = useState(null)
-  
-  const searchParams = useSearchParams();
-  const productId = searchParams.get("id");
-  const router = useRouter();
-
-  const goBack = () => {
-    if (window.history.length > 1) {
-      router.back();
-    } else {
-      router.push('/products/all'); 
-    }
-  };
-
-  useEffect(() => {
-    index.getObject(productId).then(object => {
-      setProduct(object);
-      // console.log(object);    
-    });
-  }, [])
-
+export default function MainProductPage () {
 
   return (
     <div className='py-[30px] px-[50px] max-[600px]:px-10 max-[480px]:px-5'>
-      <div className='inline-block'>
-        <button
-          onClick={goBack}
-          className='flex items-center justify-start no-underline text-[#222] text-lg gap-[3px] hover:underline'
-        >
-          <FaLessThan size={18}/> Back to Search Results
-        </button>
-      </div>
-
-      {/* Product page main */}
-      {
-        product !==null && (
-      <div className='flex items-start gap-10 py-5 max-[750px]:flex-col max-[750px]:gap-14'>
-        <ProductPageLeft product={product}/>
-        <ProductPageRight product={product} />
-      </div>
-
-      )
-      }
+      <ProductPage />
     </div>
   );
 };
--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -3,6 +3,13 @@ import Footer from "../../../components/footer";
 
 import ProductsPage from "@/components/ProductsPage/ProductsPage";
 
+export async function generateMetadata ({params}) {
+
+  return {
+    title: params.id[0].toUpperCase() + params.id.slice(1) + " ~ Products",
+    description: "It is the products page of shopmayven where you can compare, analyze and find the best products of various brands like Babyganics and cateogories like Diapering, and others according to your values like made in USA, based in New York, Female Founded, Vegan, etc.",
+  }
+}
 const MainProductsPage = ({ params }) => {
   // console.log(params);
   return (
